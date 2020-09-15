165dedefe454b85aecb5b5ebb77c084a545ab1df
Tezaen <Philippines420AD>
2020-09-15 15:08:53 -0700

    started createacctest

---
+++ app/src/androidTest/java/com/example/gradetrackerapp/CreateAccountTest.java
@@ -0,0 +1,25 @@
+package com.example.gradetrackerapp;
+
+import android.content.Context;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.example.gradetrackerapp.model.db.AppDatabase;
+import com.example.gradetrackerapp.model.db.GradeTrackerDAO;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class CreateAccountTest {
+
+    private AppDatabase db;
+    private GradeTrackerDAO mDao;
+    private Context context;
+
+    @Before
+    public void setUpDB() {
+
+    }
+
+}
