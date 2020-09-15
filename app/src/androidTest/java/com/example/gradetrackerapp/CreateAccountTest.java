d48e301db887cad49358ca6e5a11062f01598739
Tezaen <Philippines420AD>
2020-09-15 15:41:49 -0700

    Added tests to check createAccountFunctions

--- app/src/androidTest/java/com/example/gradetrackerapp/CreateAccountTest.java
+++ app/src/androidTest/java/com/example/gradetrackerapp/CreateAccountTest.java
@@ -2,13 +2,21 @@ package com.example.gradetrackerapp;
 
 import android.content.Context;
 
+import androidx.room.Room;
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.example.gradetrackerapp.model.UserLog;
 import com.example.gradetrackerapp.model.db.AppDatabase;
 import com.example.gradetrackerapp.model.db.GradeTrackerDAO;
 
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class CreateAccountTest {
@@ -19,7 +27,39 @@ public class CreateAccountTest {
 
     @Before
     public void setUpDB() {
-
+        context = ApplicationProvider.getApplicationContext();
+        db = Room.inMemoryDatabaseBuilder(context, AppDatabase.class).build();
+        mDao = db.getGradeTrackerDAO();
     }
 
+    @After
+    public void closeDB() {
+        db.close();
+    }
+
+    @Test
+    public void verifyUserNameTest() {
+        boolean pass = false;
+        UserLog testUser = new UserLog("din_djarin", "baby_yoda_ftw");
+        UserLog mUser = new UserLog("din_djarin", "baby_yoda_ftw");
+        mDao.insert(mUser);
+        UserLog dummyUser = mDao.getUserByUsername(testUser.getUsername());
+        if (dummyUser != null) {
+            pass = true;
+        }
+        assertTrue(pass);
+    }
+
+    @Test
+    public void verifyUsernameFailsTest() {
+        boolean pass = false;
+        UserLog testUser = new UserLog("din_djarinNOTTT", "baby_yoda_ftw");
+        UserLog mUser = new UserLog("din_djarin", "baby_yoda_ftw");
+        mDao.insert(mUser);
+        UserLog dummyUser = mDao.getUserByUsername(testUser.getUsername());
+        if (dummyUser != null) {
+            pass = true;
+        }
+        assertFalse(pass);
+    }
 }
