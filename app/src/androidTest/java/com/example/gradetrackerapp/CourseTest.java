9a717b69bc0347537622d0230969daa82f148bdd
Tezaen <Philippines420AD>
2020-09-17 19:11:36 -0700

    shouldve made a branch but i started courseTest

---
+++ app/src/androidTest/java/com/example/gradetrackerapp/CourseTest.java
@@ -0,0 +1,41 @@
+package com.example.gradetrackerapp;
+
+import android.content.Context;
+
+import androidx.room.Room;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.example.gradetrackerapp.model.CourseLog;
+import com.example.gradetrackerapp.model.db.AppDatabase;
+import com.example.gradetrackerapp.model.db.GradeTrackerDAO;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class CourseTest {
+    private AppDatabase db;
+    private GradeTrackerDAO mDao;
+    private Context context;
+    private CourseLog testCourse;
+
+    /**
+     * Set up DB and add a test user
+     */
+    @Before
+    public void setUpDB() {
+        context = ApplicationProvider.getApplicationContext();
+        db = Room.inMemoryDatabaseBuilder(context, AppDatabase.class).build();
+        mDao = db.getGradeTrackerDAO();
+    }
+
+    /**
+     * Close DB after test run
+     */
+    @After
+    public void closeDB() {
+        db.close();
+    }
+}
