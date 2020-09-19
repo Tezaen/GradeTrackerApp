8281c511c73f0093749bf43151019cb63bf68419
cruzy <yvcruz@csumb.edu>
2020-09-18 21:04:55 -0700

    final designs to main page

--- app/src/main/java/com/example/gradetrackerapp/MainActivity.java
+++ app/src/main/java/com/example/gradetrackerapp/MainActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button loginBtn, createBtn;
+    TextView footer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,5 +40,6 @@ public class MainActivity extends AppCompatActivity {
     private void wiredUp() {
         loginBtn = findViewById(R.id.loginBtn);
         createBtn = findViewById(R.id.createAccBtn);
+        footer = findViewById(R.id.textViewFT);
     }
 }
